8f8dcbf7870d7c2ad1cf44bfc2993c1b3808f61a
Valber <43070973+valberlimabr@users.noreply.github.com>
2024-10-24 19:10:34 -0300

    Adjust creature query for AC structure changes. (#49)

--- data/sql/db-world/base/development.sql
+++ data/sql/db-world/base/development.sql
@@ -6,8 +6,11 @@ SET
 @NpcDisplayID:=24877;
 
 DELETE FROM `creature_template` WHERE `entry` IN (@NpcEntry);
-INSERT INTO `creature_template` (`entry`, `modelid1`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `unit_class`, `unit_flags`, `type`, `type_flags`, `RegenHealth`, `flags_extra`, `ScriptName`) VALUES
-(@NpcEntry, @NpcDisplayID, @NpcName , @NpcSubname, "Speak", 0, 80, 80, 35, 1, 1, 1.14286, 1, 0, 1, 2, 7, 138936390, 1, 2, "TemplateNPC");
+INSERT INTO `creature_template` (`entry`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `unit_class`, `unit_flags`, `type`, `type_flags`, `RegenHealth`, `flags_extra`, `ScriptName`) VALUES
+(@NpcEntry, @NpcName , @NpcSubname, "Speak", 0, 80, 80, 35, 1, 1, 1.14286, 1, 0, 1, 2, 7, 138936390, 1, 2, "TemplateNPC");
+
+DELETE FROM `creature_template_model` WHERE `CreatureID` = @NpcEntry;
+INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES (@NpcEntry, 0, @NpcDisplayID, 1, 1, 0);
 
 DELETE FROM `npc_text` WHERE `ID` = @NpcEntry;
 INSERT INTO `npc_text` (`ID`, `text0_0`, `text0_1`) VALUES
